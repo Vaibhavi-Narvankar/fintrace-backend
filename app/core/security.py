b16790478bc281d32800e57daca078e6f945ce0e
vaibhavi02302 <vaibhavi@gritticon.com>
2026-06-29 12:30:57 +0530

    refactor:security update and .env.example

--- app/core/security.py
+++ app/core/security.py
@@ -47,7 +47,9 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         if email is None:
             raise HTTPException(status_code=401, detail="Invalid token")
 
-        user = db.query(User).filter(User.email == email).first()
+        user = db.query(User).filter(
+                User.email == email,
+                User.is_deleted == False).first()
 
         if user is None:
             raise HTTPException(status_code=404, detail="User not found")
@@ -55,4 +57,4 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         return user
 
     except JWTError:
-        raise HTTPException(status_code=401, detail="Invalid token")
+        raise HTTPException(status_code=401,detail="Could not validate credentials")
