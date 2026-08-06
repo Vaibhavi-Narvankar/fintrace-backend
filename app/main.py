e1a32fc4d0637192159c263fc4a453e78a687433
Vaibhavi-Narvankar <narvankarvaibhavi@gmail.com>
2026-08-06 19:50:26 +0530

    feat: logging module

--- app/main.py
+++ app/main.py
@@ -4,15 +4,19 @@ from app.db.base import Base
 from app.db.session import engine
 from app.api import router
 from app.core.exceptions import AppException
+from app.core.logging_config import setup_logging
 from app.core.exception_handlers import (
     app_exception_handler,
     validation_exception_handler,
     RequestValidationError,
     unexpected_exception_handler,
 )
+from app.middleware.request_logging import request_logging_middleware
 
+setup_logging()
 app = FastAPI()
 app.include_router(router)
+app.middleware("http")(request_logging_middleware)
 app.add_exception_handler(
     AppException,
     app_exception_handler
---
+++ app/middelware/__init__.py
@@ -0,0 +1,28 @@
+import logging
+import time
+import uuid
+from fastapi import Request
+
+logger = logging.getLogger(__name__)
+
+async def request_logging_middleware(request: Request, call_next):
+    request_id = str(uuid.uuid4())
+
+    start_time = time.perf_counter()
+
+    response = await call_next(request)
+
+    process_time = (time.perf_counter() - start_time) * 1000
+
+    logger.info(
+        "request_completed | request_id=%s | method=%s | path=%s | status_code=%s | duration_ms=%.2f",
+        request_id,
+        request.method,
+        request.url.path,
+        response.status_code,
+        process_time
+    )
+
+    response.headers["X-Request-ID"] = request_id
+
+    return response
